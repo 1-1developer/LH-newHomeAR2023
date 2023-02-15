70924130ba39b1436e3bc998cd50d8f818465cac
김용민 <gim-yongmin@gim-yongmin-ui-MacBookPro.local>
2023-02-15 23:06:04 +0900

    hangul fix01

--- Assets/Scripts/touch_debuger.cs
+++ Assets/Scripts/touch_debuger.cs
@@ -1,51 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
-public class touhOBJtest : MonoBehaviour
+public class touch_debuger : MonoBehaviour
 {
-    public UIController uIController;
     public GameObject touchedObj;
+    public Text Debugtxt;
     RaycastHit hit;
-    Pointer[] pointers;
-    Pointer pointer;
     // Start is called before the first frame update
     void Start()
     {
-        pointers = FindObjectsOfType<Pointer>();
     }
+
     private void PopUpObjectByTouch()
     {
         if (Input.touchCount > 0)
         {
-            Touch touch = Input.GetTouch(0); //?? ???? ?????? ?? ?? ???? ?????? 
-                                                //List<ARRaycastHit> hits = new List<ARRaycastHit>();
-                                                //List<RaycastHit> hits = new List<RaycastHit>();
-            Ray ray = Camera.main.ScreenPointToRay(touch.position);
 
+            Touch touch = Input.GetTouch(0);
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                
             if (Physics.Raycast(ray, out hit, 10000, LayerMask.GetMask("Pointer")))
             {
+                Debugtxt.text = "터치";
                 if (hit.transform.gameObject)
                 {
                     touchedObj = hit.transform.gameObject;
                     touchedObj.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.green);
-                    uIController.InPlanPannelAR();
-
-                    try
-                    {
-                        pointer = touchedObj.GetComponent<Pointer>();
-                    }
-                    catch (System.Exception)
-                    {
-                    }
-                    uIController.PickHighlight(uIController.buttons[pointer.HouseID]);
+                    Debugtxt.text = $"터치 :{hit.transform.gameObject.name}";
                 }
             }
+            else
+                Debugtxt.text = "터치 ray 안됨";
             
         }
         else
         {
+             Debugtxt.text = "터치x";
             if (touchedObj)
             {
                 touchedObj.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.white);
