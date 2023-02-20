8374ee3886369e607488a1887a002f0d30a4c5da
김용민 <gim-yongmin@gim-yongmin-ui-MacBookPro.local>
2023-02-20 16:39:15 +0900

    sidebutton fix

--- Assets/Scripts/UIsmartdetail.cs
+++ Assets/Scripts/UIsmartdetail.cs
@@ -12,8 +12,10 @@ public class UIsmartdetail : UIController
     Button _nextButton;
     Button _backButton_s;
 
+    int count;
 
     int smartpannelcnt = 5;
+
     protected override void SetVisualElements()
     {
         base.SetVisualElements();
@@ -22,7 +24,6 @@ public class UIsmartdetail : UIController
         {
             smartpannels.Add(m_root.Q<VisualElement>("smartpannel" + $"{i + 1}"));
             _smartbuttons.Add(m_root.Q<Button>("SmartSelectButton" + $"{i + 1}"));
-
         }
 
         _lastButton = m_root.Q<Button>("Button_last");
@@ -32,7 +33,6 @@ public class UIsmartdetail : UIController
         _lastButton.RegisterCallback<ClickEvent>(GoLast);
         _nextButton.RegisterCallback<ClickEvent>(GoNext);
         _backButton_s.RegisterCallback<ClickEvent>(GoBack);
-
 
         _smartbuttons[0].RegisterCallback<ClickEvent>(OnsmartbuttonClicked0);
         _smartbuttons[1].RegisterCallback<ClickEvent>(OnsmartbuttonClicked1);
@@ -48,41 +48,80 @@ public class UIsmartdetail : UIController
 
     private void GoNext(ClickEvent evt)
     {
-        throw new NotImplementedException();
+        count++;
+        if (count >4)
+        {
+            count = 4;
+            return;
+        }
+        openSmartpannel(smartpannels[count]);
+        updateButton(); 
     }
+
+    
 
     private void GoLast(ClickEvent evt)
     {
-        throw new NotImplementedException();
+        count--;
+        if (count < 0)
+        {
+            count = 0;
+            return;
+        }
+        openSmartpannel(smartpannels[count]);
+        updateButton();
     }
+
+    void updateButton()
+    {
+        if (count == 4)
+        {
+            _nextButton.AddToClassList("Button_next_last--un");
+        }
+        else if (count == 0)
+        {
+            _lastButton.AddToClassList("Button_next_last--un");
+        }
+        else
+        {
+            if (_nextButton.ClassListContains("Button_next_last--un"))
+                _nextButton.RemoveFromClassList("Button_next_last--un"); // 활성화
+            if (_lastButton.ClassListContains("Button_next_last--un"))
+                _lastButton.RemoveFromClassList("Button_next_last--un"); // 활성화
+        }
+    }
+
     private void OnsmartbuttonClicked0(ClickEvent evt)
     {
         AudioManager.PlayDefaultButtonSound();
+        count = 0;
         openSmartpannel(smartpannels[0]);
+        updateButton();
     }
     private void OnsmartbuttonClicked1(ClickEvent evt)
     {
         AudioManager.PlayDefaultButtonSound();
-
+        count = 1;
         openSmartpannel(smartpannels[1]);
     }
     private void OnsmartbuttonClicked2(ClickEvent evt)
     {
         AudioManager.PlayDefaultButtonSound();
-
+        count = 2;
         openSmartpannel(smartpannels[2]);
     }
     private void OnsmartbuttonClicked3(ClickEvent evt)
     {
         AudioManager.PlayDefaultButtonSound();
-
+        count = 3;
         openSmartpannel(smartpannels[3]);
     }
     private void OnsmartbuttonClicked4(ClickEvent evt)
     {
         AudioManager.PlayDefaultButtonSound();
-
+        count = 4;
         openSmartpannel(smartpannels[4]);
+        updateButton();
     }
     void openSmartpannel( VisualElement pannel)
     {
@@ -100,15 +139,10 @@ public class UIsmartdetail : UIController
             }
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("cnt: "+count);
     }
 }
